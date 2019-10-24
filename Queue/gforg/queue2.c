5d2407792236224fd7ec82805bf4c901e71a19a0
Enver <39167353+capellac@users.noreply.github.com>
2019-10-24 15:02:27 +0300

    created queue2.c

--- Queue/gforg/queue2.c
+++ Queue/gforg/queue2.c
@@ -27,14 +27,59 @@ int isEmpty(Queue *queue){
 	return (queue->size == 0);
 }
 
+// Function to add an item to the queue.  
+// It changes rear and size 
 void enqueue(Queue *queue, int data){
 	if(isFull(queue)) return;
 	queue->rear = (queue->rear +1)%queue->capacity;
 	queue->array[queue->rear] = data;
 	queue->size++;
-	printf("%d enqueued to queue", data); 
+	printf("%d enqueued to queue\n", data); 
 }
 
-int main(){
+// Function to remove an item from queue.  
+// It changes front and size 
+int dequeue(Queue* queue){
+	if(isEmpty(queue)) return INT_MIN;
+	int rValue = queue->array[queue->front];
+	queue->front = (queue->front + 1)%queue->capacity;
+	queue->size--;
+	return rValue;
+}
 
+
+// Function to get front of queue 
+int front(Queue* queue) 
+{ 
+    if (isEmpty(queue)) return INT_MIN; 
+    return queue->array[queue->front]; 
+} 
+  
+
+// Function to get rear of queue 
+int rear(Queue* queue) 
+{ 
+    if (isEmpty(queue)) return INT_MIN; 
+    return queue->array[queue->rear]; 
+} 
+
+
+void printQueue(Queue *queue){
+	if(isEmpty(queue)) return;
+	printf("your queue: \n");
+	for(int i = queue->front; i <= queue->rear; ++i){
+		printf("%d\n", queue->array[i]);
+	}
+}
+
+
+
+int main(){
+	Queue *queue = createQueue(10);
+	enqueue(queue, 10);
+	enqueue(queue, 20);
+	enqueue(queue, 30);
+	enqueue(queue, 40);
+	printf("%d dequeued from queue\n", dequeue(queue) );
+	printQueue(queue);
 }
