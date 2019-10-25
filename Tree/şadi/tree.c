c91be7c0efb6f76ab3fb0f426a95dcb1dd5288e9
Enver <39167353+capellac@users.noreply.github.com>
2019-10-25 14:27:15 +0300

    added delete function

--- Tree/şadi/tree.c
+++ Tree/şadi/tree.c
@@ -55,13 +55,40 @@ int find2(Node* tree, int data){
 //TODO: it will return the smallest value in tree.
 int findMinRecursively(Node* tree){
 	if(tree->m_left == NULL)return tree->m_data;
-	findMin(tree->m_left);
+	findMinRecursively(tree->m_left);
 }
 
 //TODO: it will return the biggest value in tree.
-int findMaxREcursively(Node* tree){
+int findMaxRecursively(Node* tree){
 	if(tree->m_right == NULL)return tree->m_data;
-	findMax(tree->m_right);
+	findMaxRecursively(tree->m_right);
+}
+
+//function to delete the given data from tree
+Node* delete(Node* tree, int data){
+	//if tree is empty
+	if(tree == NULL) return NULL;
+
+	//if we find the given data
+	if(tree->m_data == data){
+		//if it is a leaf
+		if(tree->m_right == NULL && tree->m_left == NULL)
+			return NULL;
+		if(tree->m_right != NULL){
+			tree->m_data = findMinRecursively(tree->m_right);
+			tree->m_right = delete(tree->m_right, findMinRecursively(tree->m_right));
+			return tree;
+		}
+		tree->m_data = findMaxRecursively(tree->m_left);
+		tree->m_left = delete(tree->m_left, findMaxRecursively(tree->m_left));
+		return tree;
+	}
+	if(tree->m_data < data){
+		tree->m_right = delete(tree->m_right, data);
+		return tree; 
+	}
+	tree->m_left = delete(tree->m_left, data);
+	return tree;
 }
 
 
@@ -75,8 +102,9 @@ int main(){
 	tree = add(tree, 25);
 	tree = add(tree, 40);
 	tree = add(tree, 100);
+	printf("Traverse:\n");
 	traverse(tree);
-	printf("%d\n", find2(tree, 5));
-	printf("Max value in tree: %d\n", findMinRecursively(tree));
-	printf("Min value in tree: %d\n", findMaxREcursively(tree));
+	tree = delete(tree, 20);
+	printf("Traverse:\n");
+	traverse(tree);
 }
